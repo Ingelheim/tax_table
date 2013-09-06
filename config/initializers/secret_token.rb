3ebb600cb6b578a1606003801f431e4cd83f33d9
Lukas von Ingelheim <lukas.ingelheim@gmail.com>
2013-09-06 10:30:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TaxTable::Application.config.secret_key_base = '8015d1a3377d7b8875a1e0159286fd075ee646c2f25ff4427c7939d125685805b00c9ed76eb15a10c4b629380ad97b858b70329c1c9c5238b3cf488896556377'
